158bc12caf7d36aa9d90218143ea9be1649229d1
nonameqaq1 <CHENPY29@163.COM>
2024-11-14 10:14:10 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sdm',
+    version='1.0.0',
+    description='',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'numpy',
+    ],
+)
